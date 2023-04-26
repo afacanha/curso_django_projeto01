81fe6f86b7b9c1dfa2946bd0c4e419b871bd4816
Adalberto Façanha <afacanha66@gmail.com>
2023-04-25 22:10:50 -0300

    exemplo de template temp

--- recipes/urls.py
+++ recipes/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from recipes.views import home, contato, sobre
 
 # HTTP REQUEST
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('recipes.urls')),
-    # path('recipes/', include('recipes.urls')),
+    path('', home),  # Home
+    path('sobre/', sobre),  # /sobre/
+    path('contato/', contato),  # /contato/
 ]
