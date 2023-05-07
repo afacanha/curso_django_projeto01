c58d14ae60900606cf490d8e2e13dc12d337c1d5
Adalberto Façanha <afacanha66@gamail.com>
2023-05-06 22:29:25 -0300

    Atualizado projeto para python3.11

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -3,21 +3,6 @@ from django.urls import reverse, resolve
 from recipes import views
 
 
-class RecipeURLsTest(TestCase):
-
-    def test_recipe_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipes/category/1/')
-
-    def test_recipe_detail_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipes/1/')
-
-
 class RecipeViewsTest(TestCase):
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
@@ -30,3 +15,17 @@ class RecipeViewsTest(TestCase):
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_hoem_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_hoem_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            '<h1>No recipes found here </h1>',
+            response.content.decode('utf-8'))
